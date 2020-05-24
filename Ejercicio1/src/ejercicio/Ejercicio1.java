a2f403d83b4ed58962f67803fc4e28c9b2c74d14
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-24 18:47:36 +0200

    Comienzo del programa

--- Ejercicio1/src/ejercicio/Ejercicio1.java
+++ Ejercicio1/src/ejercicio/Ejercicio1.java
@@ -1,9 +1,14 @@
 package ejercicio;
 
-
+import javax.swing.*;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        
+        FrameEjercicio1 frame = new FrameEjercicio1();
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
     }
 }
--- Ejercicio1/src/ejercicio/FrameEjercicio1.java
+++ Ejercicio1/src/ejercicio/FrameEjercicio1.java
@@ -1,7 +1,25 @@
 package ejercicio;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.File;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import javax.swing.filechooser.*;
 
+public class FrameEjercicio1 extends JFrame{
+    JButton btn1, btn2;
+    JLabel lblTeclas;
 
-public class FrameEjercicio1 {
-    
+    public FrameEjercicio1(){
+        super("Control de Ratón");
+        setLayout(null);
+
+        lblTeclas = new JLabel("Teclas");
+        lblTeclas.setSize(lblTeclas.getPreferredSize());
+        add(lblTeclas);
+    }
 }
