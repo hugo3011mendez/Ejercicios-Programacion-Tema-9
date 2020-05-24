5932d0879032d642a2d3e49931d21d3b81353f59
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-24 19:40:52 +0200

    Añadida función de cambiar título de la ventana

--- Ejercicio1/src/ejercicio/FrameEjercicio1.java
+++ Ejercicio1/src/ejercicio/FrameEjercicio1.java
@@ -10,16 +10,35 @@ import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import javax.swing.filechooser.*;
 
-public class FrameEjercicio1 extends JFrame{
+public class FrameEjercicio1 extends JFrame {
     JButton btn1, btn2;
     JLabel lblTeclas;
 
-    public FrameEjercicio1(){
+    public FrameEjercicio1() {
         super("Control de Ratón");
         setLayout(null);
+
+        class MouseHandler extends MouseAdapter { // También uso el adaptador para no escribir funciones vacías
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setTitle("Control de Ratón");
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                setTitle(String.format("Posición X:%d Y:%d ", e.getX(), e.getY()));
+            }
+        }
+
+        MouseHandler mh = new MouseHandler();
 
         lblTeclas = new JLabel("Teclas");
         lblTeclas.setSize(lblTeclas.getPreferredSize());
         add(lblTeclas);
+
+        addMouseListener(mh);
+        addMouseMotionListener(mh);
+
     }
 }
