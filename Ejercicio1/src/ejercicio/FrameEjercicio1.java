7d17aedea27787a0d8e1c13bdeea55cadb07e270
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-24 20:46:00 +0200

    Añadida función de cambiar el color de JButtons al pulsar botones del ratón

--- Ejercicio1/src/ejercicio/FrameEjercicio1.java
+++ Ejercicio1/src/ejercicio/FrameEjercicio1.java
@@ -11,14 +11,14 @@ import javax.swing.event.MouseInputListener;
 import javax.swing.filechooser.*;
 
 public class FrameEjercicio1 extends JFrame {
-    JButton btn1, btn2;
+    JButton btnClickIzquierdo, btnClickDerecho;
     JLabel lblTeclas;
 
     public FrameEjercicio1() {
         super("Control de Ratón");
-        setLayout(null);
+        setLayout(new FlowLayout());
 
-        class MouseHandler extends MouseAdapter { // También uso el adaptador para no escribir funciones vacías
+        class MouseHandler extends MouseAdapter { // Uso el adaptador para no escribir funciones vacías
 
             @Override
             public void mouseExited(MouseEvent e) {
@@ -27,18 +27,46 @@ public class FrameEjercicio1 extends JFrame {
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                setTitle(String.format("Posición X:%d Y:%d ", e.getX(), e.getY()));
+                setTitle(String.format("Control de ratón - (X:%d Y:%d)", e.getX(), e.getY()));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(e.getButton() == 1){
+                    btnClickIzquierdo.setBackground(Color.RED);
+                }
+                else if(e.getButton() == 3){
+                    btnClickDerecho.setBackground(Color.RED);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                btnClickIzquierdo.setBackground(null);
+                btnClickDerecho.setBackground(null);
             }
         }
 
-        MouseHandler mh = new MouseHandler();
+        MouseHandler mh = new MouseHandler(); // Creo el MouseHandler
+        // Añado los listeners al formulario
+        addMouseListener(mh);
+        addMouseMotionListener(mh);
 
+
+        btnClickIzquierdo = new JButton("Click Izquierdo");
+        btnClickIzquierdo.setSize(btnClickIzquierdo.getPreferredSize());
+        add(btnClickIzquierdo);
+
+        btnClickDerecho = new JButton("Click Derecho");
+        btnClickDerecho.setSize(btnClickDerecho.getPreferredSize());
+        add(btnClickDerecho);
+
+        
         lblTeclas = new JLabel("Teclas");
         lblTeclas.setSize(lblTeclas.getPreferredSize());
         add(lblTeclas);
 
-        addMouseListener(mh);
-        addMouseMotionListener(mh);
+
 
     }
 }
