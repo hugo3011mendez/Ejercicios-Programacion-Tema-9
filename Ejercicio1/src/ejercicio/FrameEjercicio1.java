a4c484a2345399597d2982b839031a4354c9d9e3
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-25 13:30:33 +0200

    Realizadas todas las funcionalidades

--- Ejercicio1/src/ejercicio/FrameEjercicio1.java
+++ Ejercicio1/src/ejercicio/FrameEjercicio1.java
@@ -10,15 +10,21 @@ import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import javax.swing.filechooser.*;
 
-public class FrameEjercicio1 extends JFrame {
+public class FrameEjercicio1 extends JFrame implements KeyListener {
     JButton btnClickIzquierdo, btnClickDerecho;
     JLabel lblTeclas;
+
+    Color colorFondoBotones = Color.RED; // Declaro una variable para gestionar el color al que cambia el fondo de los botones
 
     public FrameEjercicio1() {
         super("Control de Ratón");
         setLayout(new FlowLayout());
+        setFocusable(true);
 
         class MouseHandler extends MouseAdapter { // Uso el adaptador para no escribir funciones vacías
+
+            int xFormulario = 0;
+            int yFormulario = 0;        
 
             @Override
             public void mouseExited(MouseEvent e) {
@@ -27,16 +33,22 @@ public class FrameEjercicio1 extends JFrame {
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                setTitle(String.format("Control de ratón - (X:%d Y:%d)", e.getX(), e.getY()));
+                if(e.getSource() != btnClickIzquierdo && e.getSource() != btnClickDerecho){
+                    setTitle(String.format("Control de ratón - (X:%d Y:%d)", e.getX(), e.getY()));
+                    xFormulario = e.getX();
+                    yFormulario = e.getY();
+                }
+                else{
+                    setTitle(String.format("Control de ratón - (X:%d Y:%d)", e.getX() + xFormulario, e.getY() + yFormulario));
+                }
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                if(e.getButton() == 1){
-                    btnClickIzquierdo.setBackground(Color.RED);
-                }
-                else if(e.getButton() == 3){
-                    btnClickDerecho.setBackground(Color.RED);
+                if (e.getButton() == 1) {
+                    btnClickIzquierdo.setBackground(colorFondoBotones);
+                } else if (e.getButton() == 3) {
+                    btnClickDerecho.setBackground(colorFondoBotones);
                 }
             }
 
@@ -50,23 +62,45 @@ public class FrameEjercicio1 extends JFrame {
         MouseHandler mh = new MouseHandler(); // Creo el MouseHandler
         // Añado los listeners al formulario
         addMouseListener(mh);
-        addMouseMotionListener(mh);
-
+        addMouseMotionListener(mh);   
+        
+        addKeyListener(this); // Añado el KeyListener
 
         btnClickIzquierdo = new JButton("Click Izquierdo");
         btnClickIzquierdo.setSize(btnClickIzquierdo.getPreferredSize());
+        btnClickIzquierdo.addMouseMotionListener(mh); // También añado el MotionListener a los botones para que muestre las coordenadas aún cuando esté encima de ellos
         add(btnClickIzquierdo);
 
         btnClickDerecho = new JButton("Click Derecho");
         btnClickDerecho.setSize(btnClickDerecho.getPreferredSize());
+        btnClickDerecho.addMouseMotionListener(mh);
         add(btnClickDerecho);
 
-        
         lblTeclas = new JLabel("Teclas");
         lblTeclas.setSize(lblTeclas.getPreferredSize());
         add(lblTeclas);
 
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        lblTeclas.setText("Tecla pulsada : " + e.getKeyCode());
 
+        if (e.getKeyCode() == KeyEvent.VK_C && e.isControlDown()){
+            VentanaDeControl panelDeControl = new VentanaDeControl(this); // Declaro el formulario del panel de configuración
+            panelDeControl.pack();
+            panelDeControl.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // Y le digo que no haga nada al cerrarse para lanzar la confirmación
+            panelDeControl.setVisible(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        lblTeclas.setText(lblTeclas.getText() + ", " + e.getKeyChar());
     }
 }
